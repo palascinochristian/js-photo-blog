7ac4b5aaa845bfe62a42b0ea1d291a8410519551
palascinochristian <palascinochristian@gmail.com>
2025-01-10 13:18:11 +0100

    feat: add imgFull function to overlay

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 
+//FUNCTIONS
+const fullImg = (imgUrl) => {
+    const overlay = document.getElementById("image-overlay"); //Al richiamo della funzione seleziono l'overlay
+    overlay.querySelector("img").src = imgUrl; //Passo il valore di imgUrl all'immagine dell'overlay
+    overlay.classList.add("active"); // Aggiungo la classe active per mostrarlo
+};
+
+
 //DOM ELEMENTS
 const photoboardElm = document.getElementById("photoboard")
 
@@ -21,7 +29,13 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
             </div>`
         });
         photoboardElm.innerHTML = innerCard;
-    });
+
+        const cards = document.querySelectorAll(".card"); //Seleziono tutte le card dopo che sono state generate nel DOM
+        cards.forEach((card) => { // Per ogni card, aggiungo l'evento listener , e al click prendo il valore di .src dell'img
+            const img = card.querySelector("img");
+            card.addEventListener("click", () => fullImg(img.src)) //Al click passo il valore di img.src alla funzione fullImg che attiva l'overlay
+        })
+        });
 
 
 
